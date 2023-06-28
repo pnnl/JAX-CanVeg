d57d25c969ec6e80255436719f283512f7b0f752
Jiang, Peishi <peishi.jiang@pnnl.gov>
2023-06-28 10:51:12 -0700

    Added files to generate the python virtual environment.

---
+++ setup_env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Set up the virtual environment
+conda env create -f environment.yml
+
+# Activate the environment
+conda activate jax-watershed
+
+# Install JAX using pip
+pip install --upgrade "jax[cpu]"
+pip install diffrax
+# pip install equinox
