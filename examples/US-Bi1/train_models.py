de809a3ea01124e8cd415a2c8ab7e26700daf1e8
Jiang, Peishi <peishi.jiang@pnnl.gov>
2024-09-05 16:38:07 -0700

    Added gradient-based analysis and test code for training.

---
+++ examples/US-Bi1/train_models.py
@@ -0,0 +1,37 @@
+"""Train the models."""
+
+# import os
+import jax
+from pathlib import Path
+from jax_canveg import train_model
+from jax_canveg.shared_utilities import tune_jax_naninfs_for_debug
+
+jax.config.update("jax_enable_x64", True)
+jax.config.update("jax_traceback_filtering", "off")
+tune_jax_naninfs_for_debug(False)
+
+################################################################
+# process-based model
+################################################################
+f_config = Path("./process-based/configs.json")
+train_model(f_config)
+
+
+################################################################
+# Hybrid model 1
+################################################################
+
+
+################################################################
+# Hybrid model 2
+################################################################
+
+
+################################################################
+# Hybrid model 3
+################################################################
+
+
+################################################################
+# Pure deep learning model
+################################################################
